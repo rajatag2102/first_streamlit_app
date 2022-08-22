8d3361fc7100643b8129bf0bf5d70d71b831bd28
Rajat Agarwal <106385212+rajatag2102@users.noreply.github.com>
2022-08-22 15:29:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title ('My Parents Are New Healthy Diner')
+streamlit.text('Breakfast Menu')
+streamlit.text('Omega 3 and Blue Berry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled and Free-Range Egg')
